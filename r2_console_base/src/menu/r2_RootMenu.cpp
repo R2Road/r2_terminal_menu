3cc5aa0ffeed79c8d5296265f0baef9c8f02d28b
r2road <studior2road@gmail.com>
2022-02-11 11:50:12 +0900

    adjust key map

--- r2_console_base/src/menu/r2_RootMenu.cpp
+++ r2_console_base/src/menu/r2_RootMenu.cpp
@@ -23,19 +23,14 @@ namespace r2
 		) );
 
 		{
-			ret->AddChild( '1', key_test::Basic::GetInstance() );
+			ret->AddChild( '1', base_test::RunTest_And_Pause::GetInstance() );
+			ret->AddChild( '2', base_test::RunTest_Without_Pause::GetInstance() );
 
 			ret->AddLineFeed();
 
-			ret->AddChild( '2', base_test::RunTest_And_Pause::GetInstance() );
-			ret->AddChild( '3', base_test::RunTest_Without_Pause::GetInstance() );
-			ret->AddChild( '4', key_test::Basic::GetInstance() );
-			ret->AddChild( '5', key_test::Basic::GetInstance() );
-
-			ret->AddLineFeed();
-
+			ret->AddChild( 'q', key_test::Basic::GetInstance() );
 			ret->AddChild(
-				'q'
+				'w'
 				, []()->const char* { return r2::InputMenu::GetTitle(); }
 				, [&director]()->eTestResult
 				{
