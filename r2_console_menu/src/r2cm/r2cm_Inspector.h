9b38a86de7bd6d5b3bd2e3d993386eb7ade4696f
r2road <studior2road@gmail.com>
2022-07-23 11:57:01 +0900

    update inspector

--- r2_console_menu/src/r2cm/r2cm_Inspector.h
+++ r2_console_menu/src/r2cm/r2cm_Inspector.h
@@ -29,7 +29,7 @@ do {																							\
 do {																							\
 	if( !( condition ) )																		\
 	{																							\
-		printf( "\x1B[95m" "[PASS]" "\033[0m" " EXPECT_FALSE( %s )\n", #condition );			\
+		printf( "\x1B[34m" "[PASS]" "\033[0m" " EXPECT_FALSE( %s )\n", #condition );			\
 	}																							\
 	else																						\
 	{																							\
@@ -55,7 +55,7 @@ do {																											\
 do {																											\
 	if( ( condition_1 ) != ( condition_2 ) )																	\
 	{																											\
-		printf( "\x1B[92m" "[PASS]" "\033[0m" " EXPECT_NE( %s != %s )\n", #condition_1, #condition_2 );			\
+		printf( "\x1B[34m" "[PASS]" "\033[0m" " EXPECT_NE( %s != %s )\n", #condition_1, #condition_2 );			\
 	}																											\
 	else																										\
 	{																											\
