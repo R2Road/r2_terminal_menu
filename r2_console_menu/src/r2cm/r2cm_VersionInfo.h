14ab69bd8f41e1f2f75391832c183308c9a00c63
r2road <studior2road@gmail.com>
2022-07-23 15:15:57 +0900

    version 을 신경쓰지 않았다. 지금 부터는 사소한 변화에 라도 버전을 조정해 나가자.

--- r2_console_menu/src/r2cm/r2cm_VersionInfo.h
+++ r2_console_menu/src/r2cm/r2cm_VersionInfo.h
@@ -14,7 +14,7 @@ namespace r2cm
 
 		const char VersionNumber_1 = '1';
 		const char VersionNumber_2 = '0';
-		const char VersionNumber_3 = '0';
+		const char VersionNumber_3 = '3';
 
 		const char String4Version[15] = { 'r', '2', 'c', 'm', ' ', ':', ' ', 'v', VersionNumber_1, '.', VersionNumber_2, '.', VersionNumber_3, '\0' };
 
