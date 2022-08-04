3420f3a74e673af82e96fc59afb3a252524685a7
r2road <studior2road@gmail.com>
2022-08-04 15:43:19 +0900

    update test : test_r2cm - color_modifier_test - Basic

--- r2_console_menu/src/test_r2cm/color_modifier_test.cpp
+++ r2_console_menu/src/test_r2cm/color_modifier_test.cpp
@@ -1,5 +1,6 @@
 #include "color_modifier_test.h"
 
+#include "r2cm/r2cm_Inspector.h"
 #include "r2cm/r2cm_ostream.h"
 
 namespace color_modifier_test
@@ -18,11 +19,11 @@ namespace color_modifier_test
 			std::cout << r2cm::split;
 
 			{
-				std::cout << r2cm::tab2 << r2cm::ColorModifier( r2cm::eColor::FG_Red ) << "Do Something" << r2cm::ColorModifier() << r2cm::linefeed;
+				PROCESS_MAIN( std::cout << r2cm::tab2 << r2cm::ColorModifier( r2cm::eColor::FG_Red ) << "Do Something" << r2cm::ColorModifier() << r2cm::linefeed );
 
 				std::cout << r2cm::linefeed;
 
-				std::cout << r2cm::tab2 << r2cm::clm( r2cm::eColor::BG_Red ) << r2cm::clm( r2cm::eColor::FG_Blue ) << "Do Something" << r2cm::clm() << r2cm::linefeed;
+				PROCESS_MAIN( std::cout << r2cm::tab2 << r2cm::clm( r2cm::eColor::BG_Red ) << r2cm::clm( r2cm::eColor::FG_Blue ) << "Do Something" << r2cm::clm() << r2cm::linefeed );
 			}
 
 			std::cout << r2cm::split;
