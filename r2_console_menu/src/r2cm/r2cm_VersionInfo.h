17575b8ed2e1f10b0da8c13eb787f0b4ba3ab41f
R2Road <studior2road@gmail.com>
2022-11-24 13:32:57 +0900

    update struct : r2cm - VersionInfo - version up

--- r2_console_menu/src/r2cm/r2cm_VersionInfo.h
+++ r2_console_menu/src/r2cm/r2cm_VersionInfo.h
@@ -14,7 +14,7 @@ namespace r2cm
 
 		const char VersionNumber_1 = '1';	// 규칙 : 아래 명시된 목표 목록을 모두 완성하면 1 증가
 		const char VersionNumber_2 = '0';	// 규칙 : 아래 명시한 목표가 여러 단계로 나뉘어 있는데 한 묶음을 완성하면 1 증가
-		const char VersionNumber_3 = '2';	// 규칙 : 자잘한 작업을 진행 했고 그 것이 사용자의 코드에 어떤 변화를 유발하는 수준이면 1 증가
+		const char VersionNumber_3 = '3';	// 규칙 : 자잘한 작업을 진행 했고 그 것이 사용자의 코드에 어떤 변화를 유발하는 수준이면 1 증가
 
 		const char String4Version[14] = { 'r', '2', 'c', 'm', ' ', ':', ' ', 'v', VersionNumber_1, '.', VersionNumber_2, '.', VersionNumber_3, '\0' };
 
