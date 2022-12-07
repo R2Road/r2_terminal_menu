91453801b4cdb19823b2bebc7617c0c292e640d4
R2Road <studior2road@gmail.com>
2022-12-07 23:53:13 +0900

    ...

--- r2_console_menu/src/r2cm/r2cm_ostream.h
+++ r2_console_menu/src/r2cm/r2cm_ostream.h
@@ -5,59 +5,59 @@
 namespace r2cm
 {
 	template<class _Elem, class _Traits>
-	inline std::basic_ostream<_Elem, _Traits>& linefeed( std::basic_ostream<_Elem, _Traits>& _Ostr )
+	inline std::basic_ostream<_Elem, _Traits>& linefeed( std::basic_ostream<_Elem, _Traits>& os )
 	{
 		// insert newline
-		_Ostr.put( _Ostr.widen( '\n' ) );
-		return ( _Ostr );
+		os.put( os.widen( '\n' ) );
+		return ( os );
 	}
 
 	template<class _Elem, class _Traits>
-	inline std::basic_ostream<_Elem, _Traits>& linefeed2( std::basic_ostream<_Elem, _Traits>& _Ostr )
+	inline std::basic_ostream<_Elem, _Traits>& linefeed2( std::basic_ostream<_Elem, _Traits>& os )
 	{
-		return _Ostr << r2cm::linefeed << r2cm::linefeed;
+		return os << r2cm::linefeed << r2cm::linefeed;
 	}
 
 	template<class _Elem, class _Traits>
-	inline std::basic_ostream<_Elem, _Traits>& linefeed3( std::basic_ostream<_Elem, _Traits>& _Ostr )
+	inline std::basic_ostream<_Elem, _Traits>& linefeed3( std::basic_ostream<_Elem, _Traits>& os )
 	{
-		return _Ostr << r2cm::linefeed << r2cm::linefeed << r2cm::linefeed;
+		return os << r2cm::linefeed << r2cm::linefeed << r2cm::linefeed;
 	}
 
 
 
 	template<class _Elem, class _Traits>
-	inline std::basic_ostream<_Elem, _Traits>& split( std::basic_ostream<_Elem, _Traits>& _Ostr )
+	inline std::basic_ostream<_Elem, _Traits>& split( std::basic_ostream<_Elem, _Traits>& os )
 	{
-		_Ostr << r2cm::linefeed << "====================================================================" << r2cm::linefeed << r2cm::linefeed;
-		return ( _Ostr );
+		os << r2cm::linefeed << "====================================================================" << r2cm::linefeed << r2cm::linefeed;
+		return ( os );
 	}
 
 	template<class _Elem, class _Traits>
-	inline std::basic_ostream<_Elem, _Traits>& tab( std::basic_ostream<_Elem, _Traits>& _Ostr )
+	inline std::basic_ostream<_Elem, _Traits>& tab( std::basic_ostream<_Elem, _Traits>& os )
 	{
-		_Ostr.put( _Ostr.widen( '\t' ) );
-		return ( _Ostr );
+		os.put( os.widen( '\t' ) );
+		return ( os );
 	}
 
 	template<class _Elem, class _Traits>
-	inline std::basic_ostream<_Elem, _Traits>& tab2( std::basic_ostream<_Elem, _Traits>& _Ostr )
+	inline std::basic_ostream<_Elem, _Traits>& tab2( std::basic_ostream<_Elem, _Traits>& os )
 	{
-		_Ostr << r2cm::tab << r2cm::tab;
-		return ( _Ostr );
+		os << r2cm::tab << r2cm::tab;
+		return ( os );
 	}
 
 	template<class _Elem, class _Traits>
-	inline std::basic_ostream<_Elem, _Traits>& tab3( std::basic_ostream<_Elem, _Traits>& _Ostr )
+	inline std::basic_ostream<_Elem, _Traits>& tab3( std::basic_ostream<_Elem, _Traits>& os )
 	{
-		_Ostr << r2cm::tab << r2cm::tab << r2cm::tab;
-		return ( _Ostr );
+		os << r2cm::tab << r2cm::tab << r2cm::tab;
+		return ( os );
 	}
 
 	template<class _Elem, class _Traits>
-	inline std::basic_ostream<_Elem, _Traits>& tab4( std::basic_ostream<_Elem, _Traits>& _Ostr )
+	inline std::basic_ostream<_Elem, _Traits>& tab4( std::basic_ostream<_Elem, _Traits>& os )
 	{
-		_Ostr << r2cm::tab << r2cm::tab << r2cm::tab << r2cm::tab;
-		return ( _Ostr );
+		os << r2cm::tab << r2cm::tab << r2cm::tab << r2cm::tab;
+		return ( os );
 	}
 }
