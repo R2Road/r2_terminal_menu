6010f5ddeb8e4610850300e87995283c9f661b91
R2Road <studior2road@gmail.com>
2024-04-22 13:21:42 +0900

    version up

--- program/src/r2tm/r2tm_VersionInfo.h
+++ program/src/r2tm/r2tm_VersionInfo.h
@@ -26,7 +26,7 @@ namespace r2tm
 		const char VersionNumber_1 = '1';
 		const char VersionNumber_2 = '0';
 		const char VersionNumber_3 = '7';
-		const char VersionNumber_4 = '5';
+		const char VersionNumber_4 = '6';
 
 		const char String4Version[16] = { 'r', '2', 't', 'm', ' ', ':', ' ', 'v', VersionNumber_1, '.', VersionNumber_2, '.', VersionNumber_3, '.', VersionNumber_4, '\0' };
 
