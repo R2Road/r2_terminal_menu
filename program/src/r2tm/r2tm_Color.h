f0f1cf0decd25c8a690f35ddbab8788e15a663ca
R2Road <studior2road@gmail.com>
2025-05-09 16:47:07 +0900

    split

---
+++ program/src/r2tm/r2tm_Color.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <iostream>
+
+namespace r2tm
+{
+	enum eColor
+	{
+		None = 0,
+
+		FG_Black = 30,
+		FG_Red,
+		FG_Green,
+		FG_Yellow,
+		FG_Blue,
+		FG_Purple,
+		FG_Aqua,
+		FG_White,
+
+		FG_Gray = 90,
+		FG_LightRed,
+		FG_LightGreen,
+		FG_LightYellow,
+		FG_LightBlue,
+		FG_LightPurple,
+		FG_LightAqua,
+		FG_BrightWhite,
+
+		BG_Black = 40,
+		BG_Red,
+		BG_Green,
+		BG_Yellow,
+		BG_Blue,
+		BG_Purple,
+		BG_Aqua,
+		BG_White,
+
+		BG_Gray = 100,
+		BG_LightRed,
+		BG_LightGreen,
+		BG_LightYellow,
+		BG_LightBlue,
+		BG_LightPurple,
+		BG_LightAqua,
+		BG_BrightWhite,
+	};
+}
--- program/src/r2tm/r2tm_ColorModifier.h
+++ program/src/r2tm/r2tm_ColorModifier.h
@@ -2,49 +2,10 @@
 
 #include <iostream>
 
+#include "r2tm_Color.h"
+
 namespace r2tm
 {
-	enum eColor
-	{
-		None = 0,
-
-		FG_Black = 30,
-		FG_Red,
-		FG_Green,
-		FG_Yellow,
-		FG_Blue,
-		FG_Purple,
-		FG_Aqua,
-		FG_White,
-
-		FG_Gray = 90,
-		FG_LightRed,
-		FG_LightGreen,
-		FG_LightYellow,
-		FG_LightBlue,
-		FG_LightPurple,
-		FG_LightAqua,
-		FG_BrightWhite,
-
-		BG_Black = 40,
-		BG_Red,
-		BG_Green,
-		BG_Yellow,
-		BG_Blue,
-		BG_Purple,
-		BG_Aqua,
-		BG_White,
-
-		BG_Gray = 100,
-		BG_LightRed,
-		BG_LightGreen,
-		BG_LightYellow,
-		BG_LightBlue,
-		BG_LightPurple,
-		BG_LightAqua,
-		BG_BrightWhite,
-	};
-
 	//
 	// REF : https://stackoverflow.com/questions/2616906/how-do-i-output-coloured-text-to-a-linux-terminal
 	// 1과 2를 생각해 놓고 위 REF에 있는 3에 도달하지 못했다. Link를 남기는 것이 어떤 의미로는 굴욕이구만. - 2022.05.24 by R2Road
