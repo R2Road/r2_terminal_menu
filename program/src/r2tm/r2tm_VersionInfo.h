3075aca668f1c4fa790ef8c7594b30eedf554afc
R2Road <studior2road@gmail.com>
2025-05-09 17:07:00 +0900

    update version info : 1.7.1.4.0

--- program/src/r2tm/r2tm_VersionInfo.h
+++ program/src/r2tm/r2tm_VersionInfo.h
@@ -27,8 +27,8 @@ namespace r2tm
 		const char VersionNumber_1 = '1';
 		const char VersionNumber_2 = '7';
 		const char VersionNumber_3 = '1';
-		const char VersionNumber_4 = '3';
-		const char VersionNumber_5 = '1';
+		const char VersionNumber_4 = '4';
+		const char VersionNumber_5 = '0';
 
 		const char String4Version[18] = { 'r', '2', 't', 'm', ' ', ':', ' ', 'v', VersionNumber_1, '.', VersionNumber_2, '.', VersionNumber_3, '.', VersionNumber_4, '.', VersionNumber_5, '\0' };
 
@@ -48,6 +48,7 @@ namespace r2tm
 			"\n"	"[o] " "add method           : r2tm::SwtopWatch > PrintAverageTime_MilliSeconds, MicroSeconds, NanoSeconds, All"
 			"\n"	"[o] " "update class         : r2tm::CacheCleaner : Add > 복사 생성자 추가, 대입 연산자 제거"
 			"\n"	"[o] " "update inspector     : OUTPUT_SUBJECT: 출력 문자 변경  '+' > '#'"
+			"\n"	"[o] " "update class         : enum r2tm::eColor > enum class 적용"
 			"\n"	"[ ] " "..."
 		;
 
