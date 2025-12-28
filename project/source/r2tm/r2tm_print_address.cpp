54d75abbff6d1f6b8e2a8cc00e86cae9288b5e2b
R2Road <studior2road@gmail.com>
2025-12-28 22:07:50 +0900

    ...

--- project/source/r2tm/r2tm_print_address.cpp
+++ project/source/r2tm/r2tm_print_address.cpp
@@ -4,10 +4,16 @@ namespace r2tm
 {
 	long long CalculatePointerOffset( const void* p_1, const void* p_2 )
 	{
+		// 멤버 함수 포인터의 경우 16byte 이상이 될 수 있으므로 아래 구조체를 사용하여 주소값을 추출한다.
+		//
+		// + A::dummy 부분에 포함될 수 있는 정보
+		//   > 1. this 조정값( 상속 )
+		//   > 2. 가상 함수 테이블 조정 정보
+
 		struct A
 		{
 			long long address;
-			long long high;
+			long long dummy;
 		};
 
 		const A a_1 = ( A& )p_1;
